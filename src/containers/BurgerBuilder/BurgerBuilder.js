e3e45f8ff5c1d009f362c004c82a54a817b92ada
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2019-04-18 17:45:18 -0300

    create and implement ContactData component to handle catching the user's contact data and sending it to Firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -98,34 +98,13 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue!');
-    // this.setState({ loading: true });
-    
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'Mogul Khan',
-    //     address: {
-    //       street: 'Teststreet 123',
-    //       zipCode: '543532',
-    //       country: 'Orgrimmar'
-    //     },
-    //     email: 'test@test.com'
-    //   },
-    //   deliveryMethod: 'fastest'
-    // }
-    // axios.post('/orders.json', order)
-    //   .then(response => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   })
-    //   .catch(error => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   });
+
     const queryParams = [];
     for (let i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) + '=' +
                        encodeURIComponent(this.state.ingredients[i]));
     }
+    queryParams.push('price=' + this.state.totalPrice);
     const queryString = queryParams.join('&');
     
     this.props.history.push({
